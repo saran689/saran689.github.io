ae8c805ccffed989ab24e0f967e7a7bd81e804d0
saran689 <saran689@gmail.com>
2015-05-29 17:57:38 -0400

    P3: more comments

--- arcade-game/js/app.js
+++ arcade-game/js/app.js
@@ -15,11 +15,11 @@ var allowedTime = 1800000;
  * ENEMY our player must avoid.
  */
 var Enemy = function(speed,ypos) {
-  this.sprite = 'images/enemy-bug.png';
-  this.x = -100;
-  this.y = ypos[Math.floor(Math.random() * 3)];
-  // Speed is randomly selected from the global Speed [] using the following calculation
-  this.speed = speed[Math.floor(Math.random() * 7)];
+      this.sprite = 'images/enemy-bug.png';
+      this.x = -100;
+      this.y = ypos[Math.floor(Math.random() * 3)];
+  // Speed is randomly selected from the global enemySpeed [] using the following calculation
+      this.speed = speed[Math.floor(Math.random() * 7)];
 };
 /** 
  * Updates the enemy's position.
@@ -30,49 +30,49 @@ Enemy.prototype.update = function(dt,speed,plyr,life) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
-  this.x = this.x + (this.speed * dt);
-  if (this.x > 960) {
-    this.x = -100;
-    this.y = this.y + 83;
-    // Speed is randomly selected from the global Speed [] using the following calculation
-    this.speed = speed[Math.floor(Math.random() * 7)];
-    if (this.y > 226) {
-      this.y = 60;
-    }
-  }
+      this.x = this.x + (this.speed * dt);
+      if (this.x > 960) {
+          this.x = -100;
+          this.y = this.y + 83;
+    // Speed is randomly selected from the global enemySpeed [] using the following calculation
+          this.speed = speed[Math.floor(Math.random() * 7)];
+          if (this.y > 226) {
+              this.y = 60;
+          }
+      }
 
-  if (this.x > -50 && this.x < 50) {
-    this.tileX = 0;
-  } else if (this.x > 50 && this.x < 150) {
-    this.tileX = 101;
-  } else if (this.x > 150 && this.x < 250) {
-    this.tileX = 202;
-  } else if (this.x > 250 && this.x < 350) {
-    this.tileX = 303;
-  } else if (this.x > 350 && this.x < 450) {
-    this.tileX = 404;
-  } else if (this.x > 450 && this.x < 550) {
-    this.tileX = 505;
-  } else if (this.x > 550 && this.x < 650) {
-    this.tileX = 606;
-  } else if (this.x > 650 && this.x < 750) {
-    this.tileX = 707;
-  } else if (this.x > 750 && this.x < 850) {
-    this.tileX = 808;
-  } else if (this.x > 850) {
-    this.tileX = 1;
-  }
+      if (this.x > -50 && this.x < 50) {
+          this.tileX = 0;
+      } else if (this.x > 50 && this.x < 150) {
+          this.tileX = 101;
+      } else if (this.x > 150 && this.x < 250) {
+          this.tileX = 202;
+      } else if (this.x > 250 && this.x < 350) {
+          this.tileX = 303;
+      } else if (this.x > 350 && this.x < 450) {
+          this.tileX = 404;
+      } else if (this.x > 450 && this.x < 550) {
+          this.tileX = 505;
+      } else if (this.x > 550 && this.x < 650) {
+          this.tileX = 606;
+      } else if (this.x > 650 && this.x < 750) {
+          this.tileX = 707;
+      } else if (this.x > 750 && this.x < 850) {
+          this.tileX = 808;
+      } else if (this.x > 850) {
+          this.tileX = 1;
+      }
 
-  if (plyr.x === this.tileX && plyr.y === this.y) {
-    plyr.reset();
-    life.decrease();
-  }
+      if (plyr.x === this.tileX && plyr.y === this.y) {
+          plyr.reset();
+          life.decrease();
+      }
 };
 /**
  * Renders enemy on the screen.
  */
 Enemy.prototype.render = function(context) {
-  context.drawImage(Resources.get(this.sprite), this.x, this.y);
+      context.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
@@ -80,9 +80,9 @@ Enemy.prototype.render = function(context) {
  * Player class
  */
 var Player = function(playerimg) {
-  this.pImg = playerimg[Math.floor(Math.random() * 5)];
-  this.x = 404;
-  this.y = 392;
+      this.pImg = playerimg[Math.floor(Math.random() * 5)];
+      this.x = 404;
+      this.y = 392;
 };
 /** 
  * Updates the player's position.
@@ -90,41 +90,41 @@ var Player = function(playerimg) {
  * The player's position resets when reach the water.
  */
 Player.prototype.update = function(life) {
-  if (this.ctlKey === 'left' && this.x !== 0) {
-    this.x = this.x - 101;
-  } else if (this.ctlKey === 'right' && this.x != 808) {
-    this.x = this.x + 101;
-  } else if (this.ctlKey === 'up') {
-    this.y = this.y - 83;
-  } else if (this.ctlKey === 'down' && this.y != 392) {
-    this.y = this.y + 83;
-  }
-  this.ctlKey = null;
+      if (this.ctlKey === 'left' && this.x !== 0) {
+          this.x = this.x - 101;
+      } else if (this.ctlKey === 'right' && this.x != 808) {
+          this.x = this.x + 101;
+      } else if (this.ctlKey === 'up') {
+          this.y = this.y - 83;
+      } else if (this.ctlKey === 'down' && this.y != 392) {
+          this.y = this.y + 83;
+      }
+      this.ctlKey = null;
 
-  if (this.y < 60) {
-    this.reset();
-    life.decrease();
-  }
+      if (this.y < 60) {
+          this.reset();
+          life.decrease();
+      }
 };
 /**
  * Move the player to initial position.
  */
 Player.prototype.reset = function() {
-  this.x = 404;
-  this.y = 392;
+      this.x = 404;
+      this.y = 392;
 };
 /**
  * Renders the player on the screen.
  */
 Player.prototype.render = function(context) {
-  context.drawImage(Resources.get(this.pImg), this.x, this.y);
+      context.drawImage(Resources.get(this.pImg), this.x, this.y);
 };
 /**
  * Sets control key for updating player's postion.
  * @param key Direction passed in from user's key input.
  */
 Player.prototype.handleInput = function(key) {
-  this.ctlKey = key;
+      this.ctlKey = key;
 };
 
 
@@ -134,36 +134,38 @@ Player.prototype.handleInput = function(key) {
  * Gems appear randomly, and for every gem the player collects, 1 enemy is created.
  */
 var Gem = function(xpos,ypos,gemimg) {
-  this.gemImg = gemimg[Math.floor(Math.random() * 3)];
-  this.x = xpos[Math.floor(Math.random() * 9)];
-  this.y = ypos[Math.floor(Math.random() * 3)];
-  this.count = 0;
-  this.raise = 5;
+      this.gemImg = gemimg[Math.floor(Math.random() * 3)];
+      this.x = xpos[Math.floor(Math.random() * 9)];
+      this.y = ypos[Math.floor(Math.random() * 3)];
+      this.count = 0;
+      this.raise = 5;
 };
 /** 
  * Updates gem's position.
  * Gem's position will reset whenever player touches it.
  */
 Gem.prototype.update = function(xpos,ypos,gemimg,plyr,gamescr,glife) {
-  if (plyr.x === this.x && plyr.y === this.y) {
-    this.gemImg = gemimg[Math.floor(Math.random() * 3)];
-    this.x = xpos[Math.floor(Math.random() * 9)];
-    this.y = ypos[Math.floor(Math.random() * 3)];
-    if (allEnemies.length < 20) {
-      allEnemies.push(new Enemy(enemySpeed,ypos));
-    }
-    gamescr.score++;
-    this.count++;
-    if (gamescr.score > this.raise) {
-      gamescr.decrease(this.raise,glife);
-    }
-  }
+      //Check if player has collected a gem
+      if (plyr.x === this.x && plyr.y === this.y) {
+          this.gemImg = gemimg[Math.floor(Math.random() * 3)];
+          this.x = xpos[Math.floor(Math.random() * 9)];
+          this.y = ypos[Math.floor(Math.random() * 3)];
+          //Add more ticks to increase the difficulty level
+          if (allEnemies.length < 15) {
+              allEnemies.push(new Enemy(enemySpeed,ypos));
+          }
+          gamescr.score++;
+          this.count++;
+          if (gamescr.score > this.raise) {
+              gamescr.decrease(this.raise,glife);
+          }
+      }
 };
 /**
  * Renders the gem on the screen.
  */
 Gem.prototype.render = function(context) {
-  context.drawImage(Resources.get(this.gemImg), this.x, this.y);
+      context.drawImage(Resources.get(this.gemImg), this.x, this.y);
 };
 
 
@@ -172,72 +174,72 @@ Gem.prototype.render = function(context) {
  * 5 gems=1 life
  */
 var Score = function() {
-  this.scoreImg = 'images/Gem-Purple.jpg';
-  this.score = 0;
+      this.scoreImg = 'images/Gem-Purple.jpg';
+      this.score = 0;
 };
 
 /**
  * Decrease score by 5 and add a life.
  */
 Score.prototype.decrease = function(n,glife) {
-  if (this.score > n) {
-    this.score = this.score - n;
-    glife.life++;
-  }
+      if (this.score > n) {
+          this.score = this.score - n;
+          glife.life++;
+      }
 };
 
 /**
  * Renders Score on the screen.
  */
 Score.prototype.render = function(context) {
-  var x = 0;
-  for (var i = 0; i < this.score; i++) {
-      context.drawImage(Resources.get(this.scoreImg), x, 10);
-      x = x + 50;
-  } 
+      var x = 0;
+      for (var i = 0; i < this.score; i++) {
+          context.drawImage(Resources.get(this.scoreImg), x, 10);
+          x = x + 50;
+      } 
 };
 /**
  * LIFE of the player.
  */
 var Life = function() {
-  this.lifeImg = 'images/Heart-small.png';
-  this.life = 5;
+      this.lifeImg = 'images/Heart-small.png';
+      this.life = 5;
 };
 /**
  * Renders life on the screen.
  */
 Life.prototype.render = function(context) {
-  var x = 0;
-  for (var i = 0; i < this.life; i++) {
-    context.drawImage(Resources.get(this.lifeImg), x, 590);
-    x = x + 50;
-  }
+      var x = 0;
+      for (var i = 0; i < this.life; i++) {
+          context.drawImage(Resources.get(this.lifeImg), x, 590);
+          x = x + 50;
+      }
   
   // Game ends in allowedTime/60000 minutes or when life=0
   // To increase playtime value, change allowedTime value in line 12
-  if (this.life === 0 && msPlayedTime < allowedTime) {
-    context.drawImage(Resources.get('images/gameover.png'), 0, 15);
-    context.font = '30px Arial';
-    context.fillStyle = 'white';
-    context.fillText("You collected "+gem.count+" gems; but no lives left!",200,420);
-    context.fillText("Reload page to play again.",200,480);
-    context.drawImage(Resources.get('images/background.png'), 0, 590);
-  } else if (msPlayedTime >= allowedTime) {
-    context.drawImage(Resources.get('images/gameover.png'), 0, 15);
-    context.font = '30px Arial';
-    context.fillStyle = 'white';
-    context.fillText("You collected "+gem.count+" gems in "+allowedTime/60000.0+" minute(s)",200,420);
-    context.fillText("Reload page to play again.",200,480);
-    context.drawImage(Resources.get('images/background.png'), 0, 590);
-  }
+      if (this.life === 0 && msPlayedTime < allowedTime) {
+          context.drawImage(Resources.get('images/gameover.png'), 0, 15);
+          context.font = '30px Arial';
+          context.fillStyle = 'white';
+          context.fillText("You collected "+gem.count+" gems; but no lives left!",200,420);
+          context.fillText("Reload page to play again.",200,480);
+          context.drawImage(Resources.get('images/background.png'), 0, 590);
+      } else if (msPlayedTime >= allowedTime) {
+          context.drawImage(Resources.get('images/gameover.png'), 0, 15);
+          context.font = '30px Arial';
+          context.fillStyle = 'white';
+          context.fillText("You collected "+gem.count+" gems in "+allowedTime/60000.0+" minute(s)",200,420);
+          context.fillText("Reload page to play again.",200,480);
+          context.drawImage(Resources.get('images/background.png'), 0, 590);
+      }
 };
 /**
  * Decrease number of lives.
  */
 Life.prototype.decrease = function() {
-  if (this.life > 0) {
-    this.life = this.life - 1;
-  }
+      if (this.life > 0) {
+          this.life = this.life - 1;
+      }
 };
 
 
@@ -259,17 +261,17 @@ var gameLife = new Life();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
-  player.handleInput(allowedKeys[e.keyCode]);
+      var allowedKeys = {
+          37: 'left',
+          38: 'up',
+          39: 'right',
+          40: 'down'
+      };
+      player.handleInput(allowedKeys[e.keyCode]);
 });
 // This listens for key presses and disables default scroll actions.
 document.addEventListener('keydown', function(e) {
-  if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-    e.preventDefault();
-  }  
+      if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+          e.preventDefault();
+      }  
 }, false);
--- arcade-game/js/engine.js
+++ arcade-game/js/engine.js
@@ -1,137 +1,137 @@
 var Engine = (function(global) {
-  var doc = global.document,
-    win = global.window,
-    canvas = doc.createElement('canvas'),
-    context = canvas.getContext('2d'),
-    startTime,
-    lastTime;
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        startTime,
+        lastTime;
 
-  var playedTime = 0;
+    var playedTime = 0;
   
-  canvas.width = 909;
-  canvas.height = 850;
-  doc.body.appendChild(canvas);
+    canvas.width = 909;
+    canvas.height = 850;
+    doc.body.appendChild(canvas);
 
-  function main() {
-    var now = Date.now(),
-      dt = (now - lastTime) / 1000.0;
-    var msPlayedTime = 0;
-    update(dt);
-    render(context);
+    function main() {
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+        var msPlayedTime = 0;
+        update(dt);
+        render(context);
+        lastTime = now;
+        win.requestAnimationFrame(main);
+    }
 
-    lastTime = now;
-    win.requestAnimationFrame(main);
-  }
+    function init() {
+        reset();
+        startTime = Date.now();
+        lastTime = Date.now();
+        playedTime = 0;
+        main();
+    }
 
-  function init() {
-    reset();
-    startTime = Date.now();
-    lastTime = Date.now();
-    playedTime = 0;
-    main();
-  }
+    function update(dt) {
+        updateEntities(dt);
+        // checkCollisions();
+    }
 
-  function update(dt) {
-    updateEntities(dt);
-    // checkCollisions();
-  }
+    function updateEntities(dt) {
+        allEnemies.forEach(function(enemy) {
+            enemy.update(dt,enemySpeed,player,gameLife);
+        });
+        player.update(gameLife);
+        gem.update(gemPosX,posY,gemImages,player,gameScore,gameLife);
+    }
 
-  function updateEntities(dt) {
-    allEnemies.forEach(function(enemy) {
-      enemy.update(dt,enemySpeed,player,gameLife);
-    });
-    player.update(gameLife);
-    gem.update(gemPosX,posY,gemImages,player,gameScore,gameLife);
-  }
+    function render(ctx) {
+        var rowImages = [
+            'images/water-block.png',
+            'images/stone-block.png',
+            'images/stone-block.png',
+            'images/stone-block.png',
+            'images/grass-block.png',
+            'images/grass-block.png'
+        ],
+        numRows = 6,
+        numCols = 9,
+        row, col;
 
-  function render(ctx) {
-    var rowImages = [
+        for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
+        }
+        ctx.drawImage(Resources.get('images/background.png'), 0, 10);
+        ctx.drawImage(Resources.get('images/background.png'), 0, 590);
+        displayTime(ctx);
+        renderEntities(ctx);
+    }
+
+    function displayTime(ctx) {
+        ctx.textAlign = "left";
+        ctx.font="30px Calibri";
+        ctx.fillStyle ="#f67841";
+        ctx.strokeStyle = "#67200a";
+        ctx.lineWidth = 2;
+
+        playedTime = Date.now() - startTime;
+        msPlayedTime = parseInt(playedTime, 10);
+        playedTime = msToTime(msPlayedTime);
+        
+        ctx.fillText("Time:",700, 40);
+        ctx.strokeText("Time:",700, 40);
+        ctx.fillText(playedTime, 780, 40);
+        ctx.strokeText(playedTime, 780, 40);
+    }
+
+    function renderEntities(ctx) {
+        allEnemies.forEach(function(enemy) {
+            enemy.render(ctx);
+        });
+        player.render(ctx);
+        gem.render(ctx);
+        gameScore.render(ctx);
+        gameLife.render(ctx);
+    }
+
+    function reset() {
+        // noop
+    }
+
+    function msToTime(s) {
+
+        function addZ(n) {
+            return (n<10? '0':'') + n;
+        }
+
+        var ms = s % 1000;
+        s = (s - ms) / 1000;
+        var secs = s % 60;
+        s = (s - secs) / 60;
+        var mins = s;
+
+        return  addZ(mins) + ':' + addZ(secs) + '.' + Math.floor(ms/100.0);
+    }
+
+    Resources.load([
+        'images/background.png',
+        'images/stone-block.png',
         'images/water-block.png',
-        'images/stone-block.png',
-        'images/stone-block.png',
-        'images/stone-block.png',
         'images/grass-block.png',
-        'images/grass-block.png'
-      ],
-      numRows = 6,
-      numCols = 9,
-      row, col;
+        'images/enemy-bug.png',
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem-Orange.png',
+        'images/Gem-Blue.png',
+        'images/Gem-Green.png',
+        'images/Gem-Purple.jpg',
+        'images/Heart-small.png',
+        'images/gameover.png'
+    ]);
+    Resources.onReady(init);
 
-    for (row = 0; row < numRows; row++) {
-      for (col = 0; col < numCols; col++) {
-        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-      }
-    }
-    ctx.drawImage(Resources.get('images/background.png'), 0, 10);
-    ctx.drawImage(Resources.get('images/background.png'), 0, 590);
-    displayTime(ctx);
-    renderEntities(ctx);
-  }
-
-  function displayTime(ctx) {
-    ctx.textAlign = "left";
-    ctx.font="30px Arial";
-    ctx.fillStyle ="#F67841";
-    ctx.strokeStyle = "#67200A";
-    ctx.lineWidth = 2;
-
-    playedTime = Date.now() - startTime;
-    msPlayedTime = parseInt(playedTime, 10);
-    playedTime = msToTime(msPlayedTime);
-    ctx.fillText("Time:",700, 40);
-    ctx.strokeText("Time:",700, 40);
-    ctx.fillText(playedTime, 780, 40);
-    ctx.strokeText(playedTime, 780, 40);
-  }
-
-  function renderEntities(ctx) {
-    allEnemies.forEach(function(enemy) {
-      enemy.render(ctx);
-    });
-    player.render(ctx);
-    gem.render(ctx);
-    gameScore.render(ctx);
-    gameLife.render(ctx);
-  }
-
-  function reset() {
-    // noop
-  }
-
-  function msToTime(s) {
-
-    function addZ(n) {
-      return (n<10? '0':'') + n;
-    }
-
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s;
-
-    return  addZ(mins) + ':' + addZ(secs) + '.' + Math.floor(ms/100.0);
-  }
-
-  Resources.load([
-    'images/background.png',
-    'images/stone-block.png',
-    'images/water-block.png',
-    'images/grass-block.png',
-    'images/enemy-bug.png',
-    'images/char-boy.png',
-    'images/char-cat-girl.png',
-    'images/char-horn-girl.png',
-    'images/char-pink-girl.png',
-    'images/char-princess-girl.png',
-    'images/Gem-Orange.png',
-    'images/Gem-Blue.png',
-    'images/Gem-Green.png',
-    'images/Gem-Purple.jpg',
-    'images/Heart-small.png',
-    'images/gameover.png'
-  ]);
-  Resources.onReady(init);
-
-  global.context = context;
+    global.context = context;
 })(this);
